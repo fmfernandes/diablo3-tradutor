d9f48f915ffb9879c9743c8385f902ff1bc10c49
Fernando Fernandes <fernandofernand3s@gmail.com>
2017-06-24 03:05:30 -0300

    Adicionado o check de idade.

--- Diablo III Tradutor de Guia/translate.js
+++ Diablo III Tradutor de Guia/translate.js
@@ -1,10 +1,11 @@
-//a[class=d3_active_skill] gera "Diablo III" como nome pois é necessário passar o check de idade
+// a[class='d3_build_passive_skill_link d3_passive_skill'] apaga a imagem da skill
 
-$("a[class=d3_set], a[class=d3_legendary]").each(function(index, link) {
-  $.get($(this).attr("href").replace("en", "pt"), function(html) {
+// passar pelo check de idade
+$.post('https://us.battle.net/d3/pt/age', {day: 1, month: 3, year: 1337});
+
+$("a[class=d3_set], a[class=d3_legendary], a[class=d3_active_skill]").each(function(index, link) {
+  $.get($(this).attr("href").replace("en", "pt").replace("http", "https"), function(html) {
     var itemName = $(html).find("title").text().replace(" - Guia - Diablo III", "");
     $(link).text(itemName);
   });
 });
-
-
